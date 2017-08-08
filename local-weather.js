d758b5e5bd7d9e644914df5e6462dd18fc0a9f16
Nuanprae Gopalakrishnan <prae@Nuanpraes-MacBook-Air.local>
2017-08-09 01:37:17 +1000

    Fixed json file from api

--- local-weather.js
+++ local-weather.js
@@ -1,39 +1,20 @@
-var latitude;
-var longitude;
-
-// load json file from url
-function getJSON(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-    // Success!
-        var data = JSON.parse(request.responseText);
-        } else {
-    // We reached our target server, but it returned an error
-        }
-    };
-    request.onerror = function() {
-  // There was a connection error of some sort
-    };
-    request.send();
-}
 
 // display user location if able to retrieve location from api
 function geoSuccess(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
     var location = document.querySelector(".location");
-    var api = "api.openweathermap.org/data/2.5/weather?" 
+    var api = "https://fcc-weather-api.glitch.me/api/current?" 
     + "lat=" + latitude 
-    + "&lon=" + longitude 
-    + "&units=metric" 
-    + "&APPID=b67c754d8b9dac8134657159e3c93c8c";
-    
-    getJSON(api, function(data) {
+    + "&lon=" + longitude;
+
+// load json file from url
+fetch(api)
+.then((resp) => resp.json()) 
+.then(function(data) {
     var cityName = data.name;
     location.innerHTML = cityName;
-    });
+}) 
 }
 
 // display error message if unable to get location from API
